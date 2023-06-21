4fb133a61ae13ee52760a1fee4622ee7f65b70e6
muvlxww <136902238+muvlxww@users.noreply.github.com>
2023-06-21 10:01:37 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+// let name= "по полям по полям";
+// alert (name);
+// let age= 16;
+// alert(age);
+// let a = name+" "+age;
+// alert(a); 
+
+// let a= "2"
+// let b= "3"
+// alert(a+b)
+
+//  let a =+prompt("Введите ширину прямоугольника");
+//  let b =+prompt ("Введите длину прямоугольника");
+//  let P = (a+b)*2
+// alert("Периметр прямоугольника равен "+P);
+
+// let a= "привет ";
+// let b = "мир";
+// alert(a+b);
